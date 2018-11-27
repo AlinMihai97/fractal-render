f86b6f24cb4d37f02e9365089c3d54a53293c4e5
Alin <alinmihai97@gmail.com>
2018-11-27 14:30:28 +0200

    changed tests

--- run_test.sh
+++ run_test.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-make all
+make all &> /dev/null
 
-./fractal 500 500 200 test1.bmp
-./fractal 800 800 400 test2.bmp
+echo "Serial implementation------"
+./fractal 400 400 200 test1.bmp
+./fractal 400 400 400 test2.bmp
+./fractal 800 800 400 test3.bmp
+echo ""
+echo "OMP implementation---------"
+./fractal_omp 400 400 200 test4.bmp
+./fractal_omp 400 400 400 test5.bmp
+./fractal_omp 800 800 400 test6.bmp
+echo ""
+echo "Pthreads implementation----"
+./fractal_pthreads 400 400 200 test7.bmp 8
+./fractal_pthreads 400 400 400 test8.bmp 8
+./fractal_pthreads 800 800 400 test9.bmp 8
 
-./fractal_omp 500 500 200 test3.bmp
-./fractal_omp 800 800 400 test4.bmp
-
-./fractal_pthreads 500 500 200 test5.bmp 10
-./fractal_pthreads 800 800 400 test6.bmp 10
-
-make clean
+make clean &> /dev/null
